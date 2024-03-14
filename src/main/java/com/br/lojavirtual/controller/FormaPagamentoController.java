5aebd3d5426ce379e647c3749bacccd8a02b27ca
cristiano sales <cristianoaragaosales@gmail.com>
2024-03-13 22:30:26 -0300

    Alterações do dia: 13/03/2024:
    src/main/java/com/br/lojavirtual/controller/FormaPagamentoController.java
    src/main/java/com/br/lojavirtual/repository/FormaPagamentoRepository.java

--- src/main/java/com/br/lojavirtual/controller/FormaPagamentoController.java
+++ src/main/java/com/br/lojavirtual/controller/FormaPagamentoController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.br.lojavirtual.ExceptionLojaVirtual;
 import com.br.lojavirtual.model.FormaPagamento;
 import com.br.lojavirtual.repository.FormaPagamentoRepository;
+import com.google.gson.Gson;
 
 @RestController
 public class FormaPagamentoController {
@@ -42,6 +46,19 @@ public class FormaPagamentoController {
 		
 	}
 	
+	@ResponseBody /*Poder dar um retorno da API*/
+	@PostMapping(value = "**/deleteFormaPagamento") /*Mapeando a url para receber JSON*/
+	public ResponseEntity<String> deleteFP(@RequestBody FormaPagamento formaPagamento) throws ExceptionLojaVirtual { /*Recebe o JSON e converte pra Objeto*/
+		
+		if (formaPagamentoRepository.findById(formaPagamento.getId()).isPresent() == false) {
+			throw new ExceptionLojaVirtual("Forma de Pagamento já foi removida");
+		}
+		
+		formaPagamentoRepository.deleteById(formaPagamento.getId());
+		
+		return new ResponseEntity<String>(new Gson().toJson("Forma Pagamento Removida com sucesso"),HttpStatus.OK);
+	}	
+	
 	@ResponseBody
 	@GetMapping(value = "**/listaFormaPagamento")
 	public ResponseEntity<List<FormaPagamento>> listaFormaPagamento(){
@@ -50,4 +67,44 @@ public class FormaPagamentoController {
 		
 	}
 
+	@ResponseBody /*Poder dar um retorno da API*/
+	@GetMapping(value = "**/qtdPaginaFormaPagamento/{idEmpresa}") /*Mapeando a url para receber JSON*/
+	public ResponseEntity<Integer> qtdPagina(@PathVariable("idEmpresa") Long idEmpresa) {
+		
+		Integer qtdPagina = formaPagamentoRepository.qtdPagina(idEmpresa);
+		
+		return new ResponseEntity<Integer>(qtdPagina, HttpStatus.OK);
+	}
+	
+	@ResponseBody /*Poder dar um retorno da API*/
+	@GetMapping(value = "**/buscarPorIdFp/{id}") /*Mapeando a url para receber JSON*/
+	public ResponseEntity<FormaPagamento> buscarPorIdFp(@PathVariable("id") Long id) { 
+		
+		FormaPagamento formaPagamentoId = formaPagamentoRepository.findById(id).get();
+		
+		return new ResponseEntity<FormaPagamento>(formaPagamentoId,HttpStatus.OK);
+	}
+	
+	@ResponseBody /* Pesquisa por Descrição e empresa da Forma de Pagamento */
+	@GetMapping(value = "**/buscarPorDescricaoFormaPagamento/{descricao}/{empresaId}") /*Mapeando a url para receber JSON*/
+	public ResponseEntity<List<FormaPagamento>> buscarPorDescricao2(@PathVariable("descricao") String descricao,
+			                                                           @PathVariable("empresaId") Long empresaId) { 
+		
+		List<FormaPagamento> fp = formaPagamentoRepository.buscarFormaPagamentoDes(descricao.toUpperCase(), empresaId);
+		
+		return new ResponseEntity<List<FormaPagamento>>(fp,HttpStatus.OK);
+	}	
+	
+	@ResponseBody /*Poder dar um retorno da API*/
+	@GetMapping(value = "**/listaPorPageFormaPagamento/{idEmpresa}/{pagina}") /*Mapeando a url para receber JSON*/
+	public ResponseEntity<List<FormaPagamento>> pageFormaPagamento(@PathVariable("idEmpresa") Long idEmpresa,
+			                                             @PathVariable("pagina") Integer pagina) {
+		
+		Pageable pageable = PageRequest.of(pagina, 5, Sort.by("descricao")); 
+		
+		List<FormaPagamento> lista = formaPagamentoRepository.findPorPage(idEmpresa, pageable);
+		
+		return new ResponseEntity<List<FormaPagamento>>(lista, HttpStatus.OK);
+	}
+	
 }
--- src/main/java/com/br/lojavirtual/repository/FormaPagamentoRepository.java
+++ src/main/java/com/br/lojavirtual/repository/FormaPagamentoRepository.java
@@ -2,6 +2,7 @@ package com.br.lojavirtual.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,14 @@ public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, 
 	@Query(value = "select f from FormaPagamento f where f.empresaId.id = ?1")
 	List<FormaPagamento> findAll(Long idEmpresa);
 	
+    @Query("select fp from FormaPagamento fp where fp.empresaId.id = ?1")
+    public List<FormaPagamento> findPorPage(Long idEmpresa , Pageable pageable);
+	
+    @Query(nativeQuery = true, value = "select cast((count(1) /5) as integer) + 1 as qtdpagina from t_forma_pagamento fp where fp.empresa_id = ?1")
+    public Integer qtdPagina(Long idEmpresa);
+	
+    @Query("select fp from FormaPagamento fp where upper(trim(fp.descricao)) like %?1% and fp.empresaId.id = ?2")
+	public List<FormaPagamento> buscarFormaPagamentoDes(String descricao, Long empresaId);
+    
+    
 }
