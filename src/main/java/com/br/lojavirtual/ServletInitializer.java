3f42dcdb21e54604ac20fe4c5bc49894ff76e938
cristiano sales <cristianoaragaosales@gmail.com>
2024-03-25 22:27:27 -0300

    Alterações do dia 25/03/2024:
    src/main/java/com/br/lojavirtual/ServletInitializer.java

--- src/main/java/com/br/lojavirtual/ServletInitializer.java
+++ src/main/java/com/br/lojavirtual/ServletInitializer.java
@@ -3,6 +3,8 @@ package com.br.lojavirtual;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
